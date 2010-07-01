88b8b54d41a66e14cf4c7142c09a01935f4d4955
Oliver Tonnhofer <olt@bogosoft.com>
2010-07-01 13:34:46 +0200

    add constans for quantize methods

--- PIL/Image.py
+++ PIL/Image.py
@@ -149,6 +149,10 @@ FLOYDSTEINBERG = 3 # default
 WEB = 0
 ADAPTIVE = 1
 
+MEDIANCUT = 0
+MAXCOVERAGE = 1
+FASTOCTREE = 2
+
 # categories
 NORMAL = 0
 SEQUENCE = 1
@@ -710,6 +714,7 @@ class Image:
         # methods:
         #    0 = median cut
         #    1 = maximum coverage
+        #    2 = fast octree
 
         # NOTE: this functionality will be moved to the extended
         # quantizer interface in a later version of PIL.
