bc1e62a1656f03d769cb005e8c4eb02cc407c71f
Fredrik Lundh <fredrik@effbot.org>
2010-11-09 08:49:03 +0100

    Some minor array test improvements.

--- Tests/test_numpy.py
+++ Tests/test_numpy.py
@@ -19,45 +19,45 @@ def test_numpy_to_image():
             else:
                 data = range(100)
             a = numpy.array(data, dtype=dtype)
-            a.shape = 10, 10
+            a.shape = 20, 5
             i = Image.fromarray(a)
             if list(i.getdata()) != data:
                 print "data mismatch for", dtype
         else:
             data = range(100)
             a = numpy.array([[x]*bands for x in data], dtype=dtype)
-            a.shape = 10, 10, bands
+            a.shape = 20, 5, bands
             i = Image.fromarray(a)
             if list(i.split()[0].getdata()) != range(100):
                 print "data mismatch for", dtype
         return i
 
-    # assert_image(to_image(numpy.bool, bool=1), "1", (10, 10))
-    # assert_image(to_image(numpy.bool8, bool=1), "1", (10, 10))
+    # assert_image(to_image(numpy.bool, bool=1), "1", (5, 20))
+    # assert_image(to_image(numpy.bool8, bool=1), "1", (5, 20))
 
     assert_exception(TypeError, lambda: to_image(numpy.uint))
-    assert_image(to_image(numpy.uint8), "L", (10, 10))
+    assert_image(to_image(numpy.uint8), "L", (5, 20))
     assert_exception(TypeError, lambda: to_image(numpy.uint16))
     assert_exception(TypeError, lambda: to_image(numpy.uint32))
     assert_exception(TypeError, lambda: to_image(numpy.uint64))
 
     if sys.maxint <= 2**32:
         # PIL doesn't support int64 modes yet
-        assert_image(to_image(int), "I", (10, 10))
-        assert_image(to_image(numpy.int), "I", (10, 10))
+        assert_image(to_image(int), "I", (5, 20))
+        assert_image(to_image(numpy.int), "I", (5, 20))
 
-    assert_image(to_image(numpy.int8), "I", (10, 10))
-    assert_image(to_image(numpy.int16), "I;16", (10, 10))
-    assert_image(to_image(numpy.int32), "I", (10, 10))
+    assert_image(to_image(numpy.int8), "I", (5, 20))
+    assert_image(to_image(numpy.int16), "I;16", (5, 20))
+    assert_image(to_image(numpy.int32), "I", (5, 20))
     assert_exception(TypeError, lambda: to_image(numpy.int64))
 
-    assert_image(to_image(float), "F", (10, 10))
-    assert_image(to_image(numpy.float), "F", (10, 10))
-    assert_image(to_image(numpy.float32), "F", (10, 10))
-    assert_image(to_image(numpy.float64), "F", (10, 10))
+    assert_image(to_image(float), "F", (5, 20))
+    assert_image(to_image(numpy.float), "F", (5, 20))
+    assert_image(to_image(numpy.float32), "F", (5, 20))
+    assert_image(to_image(numpy.float64), "F", (5, 20))
 
-    assert_image(to_image(numpy.uint8, 3), "RGB", (10, 10))
-    assert_image(to_image(numpy.uint8, 4), "RGBA", (10, 10))
+    assert_image(to_image(numpy.uint8, 3), "RGB", (5, 20))
+    assert_image(to_image(numpy.uint8, 4), "RGBA", (5, 20))
 
 def test_numpy_from_image():
 
