02f9a613a5c7e4825553abc0a188feb02b2b07af
Fredrik Lundh <fredrik@effbot.org>
2011-01-05 14:07:59 +0100

    Added missing string compatibility module.

---
+++ PIL/ImageString.py
@@ -0,0 +1,20 @@
+# compatibility module
+
+def find(s, p):
+    return s.find(p)
+
+def index(s, p, o=0):
+    return s.index(p,o)
+
+def join(seq, sep):
+    return sep.join(seq)
+
+def split(s, sep=None, maxsplit=None):
+    if sep is None:
+        return s.split()
+    if maxsplit is None:
+        return s.split(sep)
+    return s.split(sep, maxsplit)
+
+def strip(s):
+    return s.strip()
