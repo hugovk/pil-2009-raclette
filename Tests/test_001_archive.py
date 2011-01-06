ed2ba3fddfb9e9353deaf2991396f9e2eb2efc8a
Fredrik Lundh <fredrik@effbot.org>
2011-01-06 03:25:54 +0100

    Ignore directories and PDF files.

--- Tests/test_001_archive.py
+++ Tests/test_001_archive.py
@@ -1,11 +1,13 @@
 import PIL
 import PIL.Image
 
-import glob, os
+import glob, os, sys
 
 for file in glob.glob("../pil-archive/*"):
+    if not os.path.isfile(file):
+        continue
     f, e = os.path.splitext(file)
-    if e in [".txt", ".ttf", ".otf", ".zip"]:
+    if e in [".pdf", ".txt", ".ttf", ".otf", ".zip"]:
         continue
     try:
         im = PIL.Image.open(file)
