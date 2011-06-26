f16444a16f7614d26c1f8ac109133b00256c0401
Fredrik Lundh <fredrik@effbot.org>
2011-06-26 13:45:46 +0200

    Ignore INI files in test image directory.

--- Tests/test_001_archive.py
+++ Tests/test_001_archive.py
@@ -7,7 +7,7 @@ for file in glob.glob("../pil-archive/*"):
     if not os.path.isfile(file):
         continue
     f, e = os.path.splitext(file)
-    if e in [".pdf", ".txt", ".ttf", ".otf", ".zip"]:
+    if e in [".ini", ".pdf", ".txt", ".ttf", ".otf", ".zip"]:
         continue
     try:
         im = PIL.Image.open(file)
